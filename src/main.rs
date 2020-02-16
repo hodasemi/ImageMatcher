c94b333fdf099714a1aee8c45ce5f3d6ada8142a
hodasemi <michaelh.95@t-online.de>
2020-02-16 21:05:23 +0100

    Start implementing threshold

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use utilities::prelude::*;
 
+use float_cmp::approx_eq;
 use image;
+use image::Pixel;
 
 use std::env;
 
@@ -22,6 +24,11 @@ fn main() -> VerboseResult<()> {
         Err(err) => create_error!(err.to_string()),
     };
 
+    let threshold: Option<f32> = match arguments.get(3) {
+        Some(value) => value.parse().ok(),
+        None => None,
+    };
+
     // make sure both images have the same width and height
     assert_eq!(
         first_image.dimensions(),
@@ -37,7 +44,30 @@ fn main() -> VerboseResult<()> {
     let mut matching_pixel_count = 0;
 
     for (first_image_pixel, second_image_pixel) in first_image.pixels().zip(second_image.pixels()) {
-        if first_image_pixel == second_image_pixel {
+        let (first_r, first_g, first_b, first_a) = first_image_pixel.channels4();
+        let (second_r, second_g, second_b, second_a) = second_image_pixel.channels4();
+
+        if approx_eq!(
+            f32,
+            rgba_u8_to_f32(first_r),
+            rgba_u8_to_f32(second_r),
+            ulps = 2
+        ) && approx_eq!(
+            f32,
+            rgba_u8_to_f32(first_g),
+            rgba_u8_to_f32(second_g),
+            ulps = 2
+        ) && approx_eq!(
+            f32,
+            rgba_u8_to_f32(first_b),
+            rgba_u8_to_f32(second_b),
+            ulps = 2
+        ) && approx_eq!(
+            f32,
+            rgba_u8_to_f32(first_a),
+            rgba_u8_to_f32(second_a),
+            ulps = 2
+        ) {
             matching_pixel_count += 1;
         }
     }
@@ -50,4 +80,8 @@ fn main() -> VerboseResult<()> {
     );
 
     Ok(())
+}
+
+fn rgba_u8_to_f32(value: u8) -> f32 {
+    value as f32 / 255.0
 }
